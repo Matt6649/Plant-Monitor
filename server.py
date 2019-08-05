a11120ff6173b1289e584adc856724b6ce485213
Matt6649 <Matt6649@users.noreply.github.com>
2019-08-05 23:04:57 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from time import ctime, sleep
+import Adafruit_ADS1x15
+
+app = Flask(__name__)
+api = Api(Flask(__name__))
+
+class Data(Resource):
+    def get(self):
+        hydration = Adafruit_ADS1x15.ADS1115().read_adc(0, gain=2/3)
+        data = {'time': ctime(), 'hydration': hydration}
+        return jsonify(data)
+
+api.add_resource(Data, '/data')
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port='80')
