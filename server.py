fc99a9d878c5eaa4bb84670cf68813c4619a154f
Matt6649 <Matt6649@users.noreply.github.com>
2019-08-11 23:25:15 +0100

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ api = Api(app)
 class Data(Resource):
     def get(self):
         hydration = Adafruit_ADS1x15.ADS1115().read_adc(0, gain=2/3)
-        data = {'hydration': hydration}
+        data = {'data':'[hydration': hydration]}
         return jsonify(data)
 
 api.add_resource(Data, '/data')
